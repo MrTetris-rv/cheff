2cf246213e65d0e1b9e5d4f80b2441947a5a8698
MrTetris-rv <sharailee@bk.ru>
2020-05-20 21:34:48 +0300

    adaptive

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.querySelector(".menu").addEventListener("touchstart", menu);
 
+
 function menu() {
   document.querySelector(".test").style.display = "block";
   document.querySelector(".menu").style.display = "none";
@@ -11,4 +12,11 @@ var output = document.getElementById("rangePrice");
 output.innerHTML = "от " + slider.value + " до ";
 slider.oninput = function () {
   output.innerHTML = "от " + this.value + " до ";
+};
+document.querySelector(".up").addEventListener("touchstart", up);
+
+function up() {
+
+  document.querySelector(".test").style.display = "none";
+  document.querySelector(".menu").style.display = "block";
 }
